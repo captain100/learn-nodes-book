a5eaf525d92db0adeffcd8fcaf05c6c02a71e286
qiushi <>
2018-03-31 15:01:07 +0800

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,6 @@ var gulp = require('gulp');
 var git = require('gulp-git');
 var runSequence = require('run-sequence');
 
-gulp.task('commit-all', function() {
-    gulp.src('./*').pipe(gitgit.add());
-    console.log('------git add ------');
-    console.log('------git commit ------');
-    git.commit();
-    console.log('------git push ------');
-    git.push();
-});
 gulp.task('git:add', function() {
     return gulp.src('.').pipe(git.add());
 });
@@ -27,3 +19,5 @@ gulp.task('git:push', function(){
 gulp.task('commit-auto', function() {
     runSequence('git:add', 'git:commit', 'git:push')
 })
+
+gulp.task('default', ['commit-auto'])
